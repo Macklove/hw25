2b49127eabdd67efff2021e6f7babd996e950427
Makulov Evgeny <makulovev@gmail.com>
2022-08-08 13:01:58 +0300

    Created project

---
+++ hw25/hw25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw25
+//
+//  Created by  Евгений on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
