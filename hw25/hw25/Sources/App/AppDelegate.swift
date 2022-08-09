2f07b12511f45f4a8470dca2159f866381abf19e
Makulov Evgeny <makulovev@gmail.com>
2022-08-09 21:02:28 +0300

    Add mvc

---
+++ hw25/hw25/Sources/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  hw25
+//
+//  Created by  Евгений on 08.08.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
